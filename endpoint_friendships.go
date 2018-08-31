abd764fc8f033c26f17c203a745a59fd8d2ad750
Hinata Yanagi <hinasssan@gmail.com>
2018-08-31 16:54:31 +0900

    divided endpoint codebase

---
+++ endpoint_friendships.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"encoding/json"
+	"strconv"
+	"strings"
+	"net/http"
+)
+
+func endpoint_friendships(writer http.ResponseWriter, request *http.Request) *http_status {
+	var subject string
+
+	err := authenticate(request, &subject)
+
+	if err!= nil {
+		return &http_status{401, err.Error()}
+	}
+
+	person_id, err := strconv.Atoi(strings.TrimPrefix(request.URL.Path, "/friendships/"))
+
+	if request.Method != "GET" && err != nil {
+		return &http_status{400, "failed to parse person id"}
+	}
+
+	switch request.Method {
+	case "GET":
+
+	case "PUT":
+		if _, err := db.Exec("INSERT INTO friendships (person_0, person_1) VALUES (?, ?)", subject, person_id); err != nil {
+			return &http_status{500, err.Error()}
+		}
+	case "DELETE":
+		if _, err := db.Exec("DELETE FROM friendships WHERE person_0 = ? AND person_1 = ?", subject, person_id); err != nil {
+			return &http_status{500, err.Error()}
+		}
+	default:
+		return &http_status{405, "method not allowed"}
+	}
+
+	rows, err := db.Query("SELECT person_1 FROM friendships WHERE person_0 = ? AND person_1 = id", subject)
+
+	if err != nil {
+		return &http_status{500, err.Error()}
+	}
+
+	friends := make([]string, 0, 16)
+
+	var person string
+
+	for rows.Next() {
+		if err := rows.Scan(&person); err != nil {
+			return &http_status{500, err.Error()}
+		}
+
+		friends = append(friends, person)
+	}
+
+	buffer, err := json.Marshal(friends)
+
+	if err != nil {
+		return &http_status{500, err.Error()}
+	}
+
+	writer.Header().Set("Content-Type", "application/json")
+	writer.Write(buffer)
+
+	return nil
+}
---
+++ endpoint_messages.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"encoding/json"
+	"strconv"
+	"strings"
+	"net/http"
+)
+
+func endpoint_messages(writer http.ResponseWriter, request *http.Request) *http_status {
+	var subject string
+
+	err := authenticate(request, &subject)
+
+	if err != nil {
+		return &http_status{401, err.Error()}
+	}
+
+	if request.Method != "GET" {
+		return &http_status{405, "method not allowed"}
+	}
+
+	channel_id, err := strconv.Atoi(strings.TrimPrefix(request.URL.Path, "/messages/"))
+
+	if err != nil {
+		return &http_status{400, "invalid channel name"}
+	}
+
+	since_id, err := strconv.Atoi(request.FormValue("since_id"))
+
+	if err != nil {
+		return &http_status{400, "invalid since_id"}
+	}
+
+	rows, err := db.Query("SELECT id, channel, author, is_event, posted_at, content FROM messages WHERE channel = ? AND id > ?", channel_id, since_id)
+
+	if err != nil {
+		return &http_status{500, err.Error()}
+	}
+
+	defer rows.Close()
+
+	var message Message
+
+	for rows.Next() {
+		err := rows.Scan(&message.Id, &message.Channel, &message.Author, &message.IsEvent, &message.PostedAt, &message.Content)
+
+		if err != nil {
+			return &http_status{500, err.Error()}
+		}
+
+		buffer, err := json.Marshal(message)
+
+		if err != nil {
+			return &http_status{500, err.Error()}
+		}
+
+		writer.Header().Set("Content-Type", "application/json")
+		writer.Write(buffer)
+	}
+
+	return nil
+}
