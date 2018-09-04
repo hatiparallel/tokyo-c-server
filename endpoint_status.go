7494807050d70295a7cfeab867f5bf039a27be1c
Hinata Yanagi <hinasssan@gmail.com>
2018-09-04 23:32:30 +0900

    implemented /status

--- endpoint_status.go
+++ endpoint_status.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"net/http"
-	// "encoding/json"
+	"fmt"
+	 "encoding/json"
 )
 
 func endpoint_status(writer http.ResponseWriter, request *http.Request) *http_status {
@@ -13,6 +14,44 @@ func endpoint_status(writer http.ResponseWriter, request *http.Request) *http_st
 	if err != nil {
 		return &http_status{401, err.Error()}
 	}
+
+	rows, err := db.Query("SELECT participations.channel, MAX(id) FROM participations, messages WHERE person = ? AND participations.channel = messages.channel GROUP BY participations.channel", subject)
+
+	if err != nil {
+		return &http_status{500, err.Error()}
+	}
+
+	var status struct {
+		FriendshipCount int
+		Latests map[int]int
+	}
+
+	status.Latests = make(map[int]int)
+
+	for rows.Next() {
+		var channel, latest_id int
+
+		if err := rows.Scan(&channel, &latest_id); err != nil {
+			return &http_status{500, err.Error()}
+		}
+
+		status.Latests[channel] = latest_id
+	}
+
+	row := db.QueryRow("SELECT COUNT(*) FROM friendships WHERE person_0 = ? OR person_1 = ?", subject, subject)
+
+	if err != nil {
+		return &http_status{500, err.Error()}
+	}
+
+	if err := row.Scan(&status.FriendshipCount); err != nil {
+		return &http_status{500, err.Error()}
+	}
+
+	buffer, err := json.Marshal(status)
+
+	writer.Header().Set("Content-Type", "application/json")
+	fmt.Fprintln(writer, string(buffer))
 
 	return nil
 }
